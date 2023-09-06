7130b81d4b4fcc3e3bb560b1b64af26c98baf3f8
Thiseas Christos Lamnidis <thisseass@gmail.com>
2023-09-06 16:13:29 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 
+VERSION = "0.1.4"
 setup(
     name="pyEager",
     packages=["pyEager"],
-    version="0.1.4",
+    version=VERSION,
     license="MIT",
     description="A simple package to read in eager results.",
     long_description=open("README.md").read(),
     author="Thiseas C. Lamnidis",
     author_email="thisseass@gmail.com",
     url="https://github.com/TCLamnidis/pyEager",
-    download_url="https://github.com/TCLamnidis/pyEager/archive/refs/tags/0.1.3.tar.gz",
+    download_url=f"https://github.com/TCLamnidis/pyEager/archive/refs/tags/{VERSION}.tar.gz",
     keywords=["python", "pandas", "nf-core", "eager", "nf-core/eager", "ancient DNA"],
     python_requires=">=3.6",
     install_requires=[
